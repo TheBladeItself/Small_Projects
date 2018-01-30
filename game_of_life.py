4a12a70cb47aad2944ab75e0b7fb19e62e257150
TheBladeItself <30702939+TheBladeItself@users.noreply.github.com>
2018-01-30 12:23:29 +0000

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -37,10 +37,10 @@ def print_gen(columns, rows, array, gen_no):
 
 #---------------------------------------------------------------------------
 
-def process_next_gen(columns, rows, curr, next):
+def process_next_gen(columns, rows, current, next):
 	for i in range(1,rows-1):
 		for j in range(1,columns-1):
-			next[i][j] = process_neighbours(i, j, curr)
+			next[i][j] = process_neighbours(i, j, current)
 
 #---------------------------------------------------------------------------
       
@@ -65,15 +65,15 @@ COLS = 39
 GENERATIONS = 100
 DELAY = 0.2
 
-thisGen = []
-nextGen = []
+this_gen = []
+next_gen = []
 
-initialise_grid(COLS, ROWS, thisGen)
-initialise_grid(COLS, ROWS, nextGen)
+initialise_grid(COLS, ROWS, this_gen)
+initialise_grid(COLS, ROWS, next_gen)
 
 for gens in range(GENERATIONS):
-	print_gen(COLS, ROWS, thisGen, gens)
-	process_next_gen(COLS, ROWS, thisGen, nextGen)
+	print_gen(COLS, ROWS, this_gen, gens)
+	process_next_gen(COLS, ROWS, this_gen, next_gen)
 	time.sleep(DELAY)
-	thisGen, nextGen = nextGen, thisGen
+	this_gen, next_gen = next_gen, this_gen
 input("Finished. Press <return> to quit.")
